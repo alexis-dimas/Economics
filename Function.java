c262a03cefa38700b3e18ba401a11780f5b9feac
Alexis Dimas <112122039+alexis-dimas@users.noreply.github.com>
2023-10-09 18:51:54 -0700

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,13 @@
+public class Function {
+   private double d;
+   private double dS;
+   private double s;
+   private double sS;
+   
+   public Function(double d, double dS, double s, double sS) {
+      this.d = d;
+      this.dS = dS;
+      this.s = s;
+      this.sS = sS;
+   }
+}
